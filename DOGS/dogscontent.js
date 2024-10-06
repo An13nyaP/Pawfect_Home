09470e3943ae7dc12f5db0235e7b3087c2dfafa7
Ananya <167615021+An13nyaP@users.noreply.github.com>
2024-10-06 21:20:24 +0530

    Add files via upload

---
+++ DOGS/dogscontent.js
@@ -0,0 +1,298 @@
+
+let goldenDoodle = [
+    {
+        id: 'gd0001',
+        image: "GoldenDoodle/1.jpg",
+        name: 'Max',
+        age: '3 years',
+        gender: 'Male',
+        Temperament: 'Playful'
+    },
+    {
+        id: 'gd0002',
+        image: "GoldenDoodle/2.jpg",
+        name: 'Bella',
+        age: '2 years',
+        gender: 'Female',
+        Temperament: 'Gentle'
+    },
+    {
+        id: 'gd0003',
+        image: "GoldenDoodle/3.jpg",
+        name: 'Charlie',
+        age: '5 years',
+        gender: 'Male',
+        Temperament: 'Loyal'
+    },
+    {
+        id: 'gd0004',
+        image: "GoldenDoodle/4.jpg",
+        name: 'Daisy',
+        age: '4 years',
+        gender: 'Female',
+        Temperament: 'Energetic'
+    },
+    {
+        id: 'gd0005',
+        image: "GoldenDoodle/5.jpg",
+        name: 'Molly',
+        age: '6 years',
+        gender: 'Female',
+        Temperament: 'Calm'
+    }
+]
+
+let lab = [
+    {
+        id: 'lb0001',
+        image: "Lab/1.jpg",
+        name: 'Buddy',
+        age: '2 years',
+        gender: 'Male',
+        Temperament: 'Friendly'
+    },
+    {
+        id: 'lb0002',
+        image: "Lab/2.jpg",
+        name: 'Lucy',
+        age: '1 year',
+        gender: 'Female',
+        Temperament: 'Curious'
+    },
+    {
+        id: 'lb0003',
+        image: "Lab/3.jpg",
+        name: 'Rocky',
+        age: '3 years',
+        gender: 'Male',
+        Temperament: 'Active'
+    },
+    {
+        id: 'lb0004',
+        image: "Lab/4.jpg",
+        name: 'Sadie',
+        age: '4 years',
+        gender: 'Female',
+        Temperament: 'Loyal'
+    },
+    {
+        id: 'lb0005',
+        image: "Lab/5.jpg",
+        name: 'Cooper',
+        age: '5 years',
+        gender: 'Male',
+        Temperament: 'Playful'
+    }
+]
+
+let pomeranian = [
+    {
+        id: 'pmr0001',
+        image: "pomeranian/1.jpg",
+        name: 'Oscar',
+        age: '2 years',
+        gender: 'Male',
+        Temperament: 'Cheerful'
+    },
+    {
+        id: 'pmr0002',
+        image: "pomeranian/2.jpg",
+        name: 'Luna',
+        age: '3 years',
+        gender: 'Female',
+        Temperament: 'Affectionate'
+    },
+    {
+        id: 'pmr0003',
+        image: "pomeranian/3.jpg",
+        name: 'Milo',
+        age: '1 year',
+        gender: 'Male',
+        Temperament: 'Curious'
+    },
+    {
+        id: 'pmr0004',
+        image: "pomeranian/4.jpg",
+        name: 'Sophie',
+        age: '4 years',
+        gender: 'Female',
+        Temperament: 'Gentle'
+    },
+    {
+        id: 'pmr0005',
+        image: "pomeranian/5.jpg",
+        name: 'Leo',
+        age: '5 years',
+        gender: 'Male',
+        Temperament: 'Calm'
+    },
+    {
+        id: 'pmr0006',
+        image: "pomeranian/6.jpg",
+        name: 'Chloe',
+        age: '6 years',
+        gender: 'Female',
+        Temperament: 'Loyal'
+    }
+]
+
+let Husky = [
+    {
+        id: 'hsk0001',
+        image: "Husky/1.jpg",
+        name: 'Zeus',
+        age: '3 years',
+        gender: 'Male',
+        Temperament: 'Energetic'
+    },
+    {
+        id: 'hsk0002',
+        image: "Husky/2.jpg",
+        name: 'Athena',
+        age: '4 years',
+        gender: 'Female',
+        Temperament: 'Brave'
+    },
+    {
+        id: 'hsk0003',
+        image: "Husky/3.jpg",
+        name: 'Thor',
+        age: '2 years',
+        gender: 'Male',
+        Temperament: 'Loyal'
+    },
+    {
+        id: 'hsk0004',
+        image: "Husky/4.jpg",
+        name: 'Freya',
+        age: '5 years',
+        gender: 'Female',
+        Temperament: 'Playful'
+    },
+    {
+        id: 'hsk0005',
+        image: "Husky/5.jpg",
+        name: 'Apollo',
+        age: '6 years',
+        gender: 'Male',
+        Temperament: 'Calm'
+    },
+    {
+        id: 'hsk0006',
+        image: "Husky/6.jpg",
+        name: 'Hera',
+        age: '4 years',
+        gender: 'Female',
+        Temperament: 'Gentle'
+    }
+]
+
+let poodle = [
+    {
+        id: 'pd0001',
+        image: "poodle/1.jpg",
+        name: 'Oliver',
+        age: '2 years',
+        gender: 'Male',
+        Temperament: 'Cheerful'
+    },
+    {
+        id: 'pd0002',
+        image: "poodle/2.jpg",
+        name: 'Lily',
+        age: '3 years',
+        gender: 'Female',
+        Temperament: 'Gentle'
+    },
+    {
+        id: 'pd0003',
+        image: "poodle/3.jpg",
+        name: 'Duke',
+        age: '1 year',
+        gender: 'Male',
+        Temperament: 'Playful'
+    },
+    {
+        id: 'pd0004',
+        image: "poodle/4.jpg",
+        name: 'Maya',
+        age: '4 years',
+        gender: 'Female',
+        Temperament: 'Loyal'
+    },
+    {
+        id: 'pd0005',
+        image: "poodle/5.jpg",
+        name: 'Max',
+        age: '5 years',
+        gender: 'Male',
+        Temperament: 'Calm'
+    },
+    {
+        id: 'pd0006',
+        image: "poodle/6.jpg",
+        name: 'Zoe',
+        age: '6 years',
+        gender: 'Female',
+        Temperament: 'Affectionate'
+    },
+    {
+        id: 'pd0007',
+        image: "poodle/7.jpg",
+        name: 'Jack',
+        age: '4 years',
+        gender: 'Male',
+        Temperament: 'Friendly'
+    }
+]
+
+let germanshepherd = [
+    {
+        id: 'gmsp0001',
+        image: "german shepherd/1.jpg",
+        name: 'Rex',
+        age: '3 years',
+        gender: 'Male',
+        Temperament: 'Protective'
+    },
+    {
+        id: 'gmsp0002',
+        image: "german shepherd/2.jpg",
+        name: 'Bella',
+        age: '2 years',
+        gender: 'Female',
+        Temperament: 'Loyal'
+    },
+    {
+        id: 'gmsp0003',
+        image: "german shepherd/3.jpg",
+        name: 'Rocky',
+        age: '4 years',
+        gender: 'Male',
+        Temperament: 'Brave'
+    },
+    {
+        id: 'gmsp0004',
+        image: "german shepherd/4.jpg",
+        name: 'Nala',
+        age: '5 years',
+        gender: 'Female',
+        Temperament: 'Energetic'
+    },
+    {
+        id: 'gmsp0005',
+        image: "german shepherd/5.jpg",
+        name: 'Hunter',
+        age: '6 years',
+        gender: 'Male',
+        Temperament: 'Alert'
+    },
+    {
+        id: 'gmsp0006',
+        image: "german shepherd/6.jpg",
+        name: 'Mila',
+        age: '4 years',
+        gender: 'Female',
+        Temperament: 'Friendly'
+    }
+]
